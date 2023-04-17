5365ff3e2cb01c3e558997ef36bbf841f8ec85c1
leroysb <leroybuliro@gmail.com>
2023-04-17 16:31:05 +0300

    pwa.urls mod

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,21 +1,20 @@
-"""pwa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
+    path('account/',include('account.urls')),
+    path('', include('core.urls')),
+    # path('shop/', include('store.urls')),
 ]
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'core.errorviews.page404'
+handler500 = 'core.errorviews.page500'
+handler400 = 'core.errorviews.page404' # Bad request
+# handler403 = 'mysite.views.my_custom_permission_denied_view' # Permission denied
